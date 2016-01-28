c81f233001705c8be882b744bae13415b395c21f
John Pangalos <jpangalos@iotum.com>
2016-01-28 01:09:30 -0500

    fixing it so gulp will build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,21 @@
+'use strict';
+
 $('#home').click(function(){
   $('html, body').animate({
-      scrollTop: $("html").offset().top
+      scrollTop: $('html').offset().top
   }, 2000);
 });
 
 $('#show_info').click(function(){
-  var second_panel_position = $(".second-panel").offset().top - $(".second-panel").css("padding-top").replace("px", "");
+  var secondPanelPosition = $('.second-panel').offset().top - $('.second-panel').css('padding-top').replace('px', '');
   $('html, body').animate({
-    scrollTop: second_panel_position
+    scrollTop: secondPanelPosition
   }, 2000);
 });
 
 $('#credits').click(function(){
-  var second_panel_position = $(".third-panel").offset().top - $(".third-panel").css("padding-top").replace("px", "");
+  var thirdPanelPosition = $('.third-panel').offset().top - $('.third-panel').css('padding-top').replace('px', '');
   $('html, body').animate({
-    scrollTop: second_panel_position
+    scrollTop: thirdPanelPosition
   }, 2000);
 });
